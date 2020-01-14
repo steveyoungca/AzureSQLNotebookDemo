b925eb29c3dfc19d52dfbb192ce83a982af157c9
steveyoungca <steveyoungca@users.noreply.github.com>
2020-01-14 15:40:18 -0500

    These are the new files based on the second demo

---
+++ Demo3_AzureDataStudio_AzureSQLNotebooks/Demo3_TestSQL.sql
@@ -0,0 +1,25 @@
+﻿---''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+---  SQL SCRIPT: Demo3_TestSQL.sql
+---
+--- Description: Demo of data virtualization - 
+---		 
+---
+--- Parameters :   
+---
+---
+--- Date               Developer            Action
+--- ---------------------------------------------------------------------
+--- Nov 05, 2019       Steve Young          Writing And Development
+---
+---
+---
+/*
+
+*/
+SELECT TOP (1000) [VehicleTemperatureID]
+      ,[VehicleRegistration]
+      ,[ChillerSensorNumber]
+      ,[RecordedWhen]
+      ,[Temperature]
+      ,[FullSensorData]
+  FROM [WideWorldImporters].[Website].[VehicleTemperatures]
